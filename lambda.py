f9913fbef7d0166bd1286327f47c6ae99b6048ea
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2022-12-21 18:49:30 -0300

    Add Lambda function to handle webhooks

---
+++ lambda.py
@@ -0,0 +1,49 @@
+# 
+# VAZ Projects
+# 
+# 
+# Author: Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
+
+
+
+import os
+import boto3
+import json
+import logging
+
+
+
+def main( event, context ):
+	logging.getLogger().setLevel( logging.INFO )
+	logging.info( event )	# TEMP
+	
+	
+	# Env vars from Terraform.
+	secretToken = os.environ['secretToken']
+	
+	
+	# Validate request.
+	try:
+		gitlabEventData = json.loads( event['body'] )
+		jobStatus = gitlabEventData['build_status']
+	except:
+		logging.info( f'Invalid request.' )
+		return { 'statusCode': 400 }
+	
+	
+	# Authorization.
+	if event['headers']['x-gitlab-token'] != secretToken:
+		logging.info( f'Invalid secret token.' )
+		return { 'statusCode': 400 }
+	
+	
+	# Check job status.
+	logging.info( f'Job event: {jobStatus}' )
+	if jobStatus != 'created':
+		return { 'statusCode': 200 }
+	
+	
+	# Launch runner.
+	logging.info( f'Launching runner...' )
+	
+	return { 'statusCode': 200 }
---
+++ lambda.tf
@@ -0,0 +1,117 @@
+# 
+# VAZ Projects
+# 
+# 
+# Author: Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
+
+
+locals {
+	lambda_function_name = "${var.prefix}-${var.identifier}-lambda"	# Avoid cyclic dependency.
+}
+
+# 
+# Lambda Function.
+#-------------------------------------------------------------------------------
+resource "aws_lambda_function" "lambda" {
+	function_name = local.lambda_function_name
+	role = aws_iam_role.lambda_role.arn
+	
+	runtime = "python3.9"
+	filename = data.archive_file.lambda.output_path
+	source_code_hash = data.archive_file.lambda.output_base64sha256
+	handler = "lambda.main"
+	timeout = 10
+	
+	environment {
+		variables = {
+			secretToken = ""
+		}
+	}
+	
+	# Make sure the log group is created before the function because we removed the implicit dependency.
+	depends_on = [ aws_cloudwatch_log_group.lambda_log_group ]
+	
+	tags = {
+		Name = "${var.name} Lambda"
+	}
+}
+
+
+data "archive_file" "lambda" {
+	type = "zip"
+	source_file = "${path.module}/lambda.py"
+	output_path = "deployment/${var.prefix}/${var.identifier}/lambda.zip"
+}
+
+
+resource "aws_lambda_function_url" "lambda_url" {
+	function_name = aws_lambda_function.lambda.function_name
+	authorization_type = "NONE"
+}
+
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+	name = "/aws/lambda/${local.lambda_function_name}"
+	
+	tags = {
+		Name = "${var.name} Lambda Log Group"
+	}
+}
+
+
+
+# 
+# Lambda IAM Role.
+#-------------------------------------------------------------------------------
+resource "aws_iam_role" "lambda_role" {
+	name = "${var.prefix}-${var.identifier}-lambdaRole"
+	assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+	
+	inline_policy {
+		name = "${var.prefix}-${var.identifier}-lambdaRolePolicy"
+		
+		policy = data.aws_iam_policy_document.lambda_role_policy.json
+	}
+	
+	tags = {
+		Name: "${var.name} Lambda Role"
+	}
+}
+
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+	statement {
+		sid = "lambdaAssumeRole"
+		
+		principals {
+			type = "Service"
+			identifiers = [ "lambda.amazonaws.com" ]
+		}
+		
+		actions = [ "sts:AssumeRole" ]
+	}
+}
+
+
+data "aws_iam_policy_document" "lambda_role_policy" {
+	# Used in lambda.py.
+	statement {
+		sid = "ec2DescribeInstances"
+		
+		actions = [ "ec2:DescribeInstances" ]
+		
+		resources = [ "*" ]
+	}
+	
+	# Used by Lambda.
+	statement {
+		sid = "cloudwatchWriteLogs"
+		
+		actions = [
+			"logs:CreateLogStream",
+			"logs:PutLogEvents",
+		]
+		
+		resources = [ "${aws_cloudwatch_log_group.lambda_log_group.arn}:*" ]
+	}
+}
